6e23af41c0fcb7472ac3cf0bf696cbc51880eacb
tusharkeshav <ruletushar@gmail.com>
2022-11-12 21:48:01 +0530

    Added prop file. But need to removed as it has api keys

---
+++ ApplicationProperties.py
@@ -0,0 +1,12 @@
+
+ngrok_secret="2Gtfl9p8fICyKmcYofbV8CnOXMw_6iZCf9KCAv7fqHiRGD7uv"
+BASE_TUNNEL_URL = 'https://api.ngrok.com/tunnels'
+
+# Use Cut.ly instead
+cutt_ly_secret = 'd07364e03b5eb505576278d7362965100d3a2'
+BASE_API_URL = 'http://cutt.ly/api/api.php'
+BASE_DOMAIN = 'https://cutt.ly/'
+
+SSH_custom_alias = 'testxyz12'  # it will be like https://cutt.ly/testxyz12
+binary_path = '/home/akhil/Documents/ngrok-2.2.2-linux-amd64/ngrok'
+
